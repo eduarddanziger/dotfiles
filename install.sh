4ecc943b85765bd4ab17894587e4ae3aea44987f
Eduard D. <180659468+eduarddanziger@users.noreply.github.com>
2025-02-23 18:37:04 +0100

    Correct install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Symlink dotfiles to the home directory
-ln -sf ~/dotfiles/.bashrc ~/.bashrc
+ln -sf /workspaces/.codespaces/.persistedshare/dotfiles/.bashrc ~/.bashrc
 
 # Reload the shell configuration
 source ~/.bashrc
